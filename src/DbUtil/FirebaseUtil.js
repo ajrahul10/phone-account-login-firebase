f37aa528861702a7c74633966547313c0c637827
Rahul Ajmani <rahulajmani910@gmail.com>
2021-02-01 21:06:37 +0530

    adding ref of ccmongo doc-id in ccfirestore

--- src/DbUtil/FirebaseUtil.js
+++ src/DbUtil/FirebaseUtil.js
@@ -1,10 +1,8 @@
 import firebase from '../firebase';
-import { v4 as uuidv4 } from 'uuid';
 
 // Firestore CRUD utility file
 export async function fetchUser(phoneNum) {
     const db = firebase.firestore();
-    console.log(phoneNum);
     let res = null;
 
     // checking if user with the mobile number already exists or not
@@ -16,7 +14,6 @@ export async function fetchUser(phoneNum) {
             if(mmid === phoneWithoutPlus) {
                 res = doc.data();
             }
-            // console.log(doc.data());
         });
     });
     return res;
@@ -25,37 +22,25 @@ export async function fetchUser(phoneNum) {
 export async function addUser(firstname, middlename, lastname, mmid) {
 
     const db = firebase.firestore();
-    const uuidKey = uuidv4();
 
     mmid = mmid.substring(1);
 
     // adding user details to firestore ccmongo collection with doc-id as uuidKey
-    db.collection("ccmongo").doc(uuidKey).set({
+    const {id} = await db.collection("ccmongo").add({
         frstname: firstname,
         middlename: middlename,
         lastname: lastname,
         fullname: firstname + " " + middlename + " " + lastname,
         mmid: mmid
     })
-    .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
-    });
 
 
     // adding user details to firebase ccfirestore collection 
-    // refering the same uuidKey from ccmongo over here
-    db.collection("ccfirestore").add({
+    // refering the same doc-id from ccmongo over here
+    const {id1} = await db.collection("ccfirestore").add({
         fullname: firstname + " " + middlename + " " + lastname,
         mmid: mmid,
-        mongoid: uuidKey
+        mongoid: id
     })
-    .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
-    });
+
 }
